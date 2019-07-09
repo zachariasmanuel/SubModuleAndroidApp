3d715228d07bd2669db2a78d795f6160402880f1
zachariasmanuel <zac@tricog.com>
2019-07-09 13:39:56 +0530

    Edited algorithm

---
+++ submodule/src/main/java/com/example/submoduleandroidapp/Algorithm.kt
@@ -0,0 +1,10 @@
+package com.example.submoduleandroidapp
+
+class Algorithm {
+
+    companion object {
+        fun sum(a: Int, b: Int): Int {
+            return a + b
+        }
+    }
+}
--- submodule/src/main/java/com/example/submoduleandroidapp/MainActivity.kt
+++ submodule/src/main/java/com/example/submoduleandroidapp/MainActivity.kt
@@ -5,15 +5,21 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        textView = findViewById(R.id.text_view)
+        textView.text = "Answer is "+Algorithm.sum(3,5)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
